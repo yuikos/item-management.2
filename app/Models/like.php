533f81f4d960b429a421080b146d3fefd1f71287
yuikos <yuiko.niwa.1993@gmail.com>
2022-11-20 09:36:16 +0900

    like to Like

--- app/Models/like.php
+++ app/Models/like.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\DB;
 
-class like extends Model
+class Like extends Model
 {
     use HasFactory;
 
