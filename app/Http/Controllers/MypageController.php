f49da0fe42dd26c590a29262e2ec5837efc20f73
yuikos <yuiko.niwa.1993@gmail.com>
2022-11-20 09:29:18 +0900

    like to Like

--- app/Http/Controllers/MypageController.php
+++ app/Http/Controllers/MypageController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\Item;
-use App\Models\like;
+use App\Models\Like;
 
 class MypageController extends Controller
 {
--- app/Http/Controllers/RankController.php
+++ app/Http/Controllers/RankController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use App\Models\Item;
-use App\Models\like;
+use App\Models\Like;
 use App\Models\Keyword;
 
 class RankController extends Controller
